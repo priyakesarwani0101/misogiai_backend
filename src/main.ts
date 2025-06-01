6aeab7041b87a066c5bc25331a88e573f4edf5b5
Priya Kesarwani <priyakesarwani0101@gmail.com>
2025-06-01 15:40:36 +0530

    cors fixed

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,12 @@ async function bootstrap() {
   // app.use(cookieParser());
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5174',
+    origin: [
+      process.env.FRONTEND_URL,
+      'https://misogiai-dkka.onrender.com',
+      'https://misogiai-frontend.onrender.com',
+      'http://localhost:5173',
+    ],
     credentials: true,
   });
 
